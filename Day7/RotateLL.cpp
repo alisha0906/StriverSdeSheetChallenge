cd0f12bec86bd19fd096a61a53ebd880d21901aa
alisha0906 <116641855+alisha0906@users.noreply.github.com>
2023-06-06 19:38:52 +0530

    Update RotateLL.cpp

--- Day7/RotateLL.cpp
+++ Day7/RotateLL.cpp
@@ -1,1 +1,35 @@
+ListNode* rotateRight(ListNode* head, int k) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    if (k == 0) {
+        return head;
+    }
+    
+    ListNode* temp = head;
+    int count = 1; // Start count at 1 since head is not NULL
+    
+    while (temp->next != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    if (k % count == 0) {
+        return head;
+    }
+    
+    temp->next = head; // Connect the tail to the head to form a circular list
+    
+    int rotateSteps = count - (k % count);
+    temp = head;
+    
+    for (int i = 0; i < rotateSteps - 1; i++) {
+        temp = temp->next;
+    }
+    
+    head = temp->next;
+    temp->next = NULL;
+    
+    return head;
+}
 
